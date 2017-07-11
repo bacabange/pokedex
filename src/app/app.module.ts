727057745a7fce93da5357d0d170715bd1e783e7
Stiven Castillo <stivencastillo.90@gmail.com>
2017-07-11 10:07:17 -0500

    View home and pokemon list

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,30 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { routing, appRoutingProviders} from './app.routing';
+import { MaterialModule } from '@angular/material';
 
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { MenuComponent } from './shared/menu/menu.component';
+import { SearchInputComponent } from './shared/search-input/search-input.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    MenuComponent,
+    SearchInputComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    routing,
+    MaterialModule
   ],
-  providers: [],
+  providers: [
+  	appRoutingProviders
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
---
+++ src/app/services/pokeapi/pokeapi.service.ts
@@ -0,0 +1,21 @@
+import { Injectable } from '@angular/core';
+import { Http, Response, Headers, URLSearchParams, QueryEncoder, RequestOptions } from '@angular/http';
+import { Observable } from 'rxjs/Rx';
+import 'rxjs/add/operator/map';
+import 'rxjs/add/operator/catch';
+
+@Injectable()
+export class PokeapiService {
+
+	public url:string;
+
+	constructor() {
+		this.url = 'http://pokeapi.co/';
+	}
+
+
+	getAllPokemons() {
+		let url = this.url + 'api/v1/pokedex/1/';
+	}
+
+}
