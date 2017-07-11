d23f4de92ed3f37579c929cdda0c462b43341346
Stiven Castillo <stiven.castillo@creardigital.com>
2017-07-11 10:27:55 -0500

    Fixed conflicts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,8 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-<<<<<<< HEAD
-=======
 import { routing, appRoutingProviders} from './app.routing';
 import { MaterialModule } from '@angular/material';
->>>>>>> 727057745a7fce93da5357d0d170715bd1e783e7
 
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
@@ -21,16 +18,12 @@ import { SearchInputComponent } from './shared/search-input/search-input.compone
   ],
   imports: [
     BrowserModule,
-<<<<<<< HEAD
-    BrowserAnimationsModule
-=======
     BrowserAnimationsModule,
     routing,
     MaterialModule
   ],
   providers: [
   	appRoutingProviders
->>>>>>> 727057745a7fce93da5357d0d170715bd1e783e7
   ],
   bootstrap: [AppComponent]
 })
