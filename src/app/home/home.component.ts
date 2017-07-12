a98449b3ee9b05d8110a200476d4ab8a73cd28cc
Stiven Castillo <stiven.castillo@creardigital.com>
2017-07-12 17:58:03 -0500

    Pokemon and moves Models

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,17 +1,26 @@
 import { Component, OnInit } from '@angular/core';
 import { MenuComponent } from '../shared/menu/menu.component';
 import { SearchInputComponent } from '../shared/search-input/search-input.component';
+import { PokeapiService } from '../services/pokeapi/pokeapi.service';
+
+import Pokemon from '../models/pokemon';
 
 @Component({
 	selector: 'app-home',
 	templateUrl: './home.component.html',
 	styleUrls: ['./home.component.css'],
+	providers: [
+		PokeapiService
+	]
 })
 export class HomeComponent implements OnInit {
 
-	constructor() { }
+	private allPokemon: Array<Pokemon> = [];
+
+	constructor(public service: PokeapiService) { }
 
 	ngOnInit() {
+		
 	}
 
 }
--- src/app/services/pokeapi/pokeapi.service.ts
+++ src/app/services/pokeapi/pokeapi.service.ts
@@ -3,6 +3,8 @@ import { Http, Response, Headers, URLSearchParams, QueryEncoder, RequestOptions 
 import { Observable } from 'rxjs/Rx';
 import 'rxjs/add/operator/map';
 import 'rxjs/add/operator/catch';
+import Pokemon from '../../models/pokemon';
+import Move from '../../models/move';
 
 @Injectable()
 export class PokeapiService {
